812eeb81bae78659e4f03b6dc9f367bdf4b46b96
DESKTOP-8VD9HUF\Aaron <AaronPBrooks@outlook.com>
2017-02-22 18:50:27 -0500

    Initial project setup for the neural net project

---
+++ RoboGeneral6000/RoboGeneral6000/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboGeneral6000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
