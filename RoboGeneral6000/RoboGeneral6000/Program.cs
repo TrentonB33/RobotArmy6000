eb7ce474b3fa54d45fb4cd2c72148a85776a541e
Trenton Baldrey <trentmant@gmail.com>
2017-03-03 21:36:06 -0500

    Added structure to store the game frame data, and am hopefully parsing the JSON correctly.

--- RoboGeneral6000/RoboGeneral6000/Program.cs
+++ RoboGeneral6000/RoboGeneral6000/Program.cs
@@ -2,35 +2,78 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using WebSocketSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Diagnostics;
 
 //The websocket I'm using is from the following source:
 //https://github.com/sta/websocket-sharp
 
 namespace RoboGeneral6000
 {
-    class Program
+    class RoboSystem
     {
         static void Main(string[] args)
         {
             
-            String url = "ws://npcompete.io/wsjoin?game=GAMENAME";
+            String url = "ws://npcompete.io/wsjoin?game=PenTest";
             String devkey = "AustinsLoudlyMagnificentRamen";
+
+            String gameState;
+
+            FileStream output = File.Create("testText.txt");
+
+            RoboData currentState;
+
+            int counter = 0;
+            int maxLines = 100;
+            
+            /*****************************************************************
+            //TODO: Go and look at the json to csharp website and also get the
+            //Game to send me more inormation on units and towers so i can make a better
+            //json file to give it
+            /****************************************************************/
 
             var socket = new WebSocket(url);
 
             socket.OnMessage += (sender, e) =>
-                Console.WriteLine("Data: " + e.Data);
+            {
+                gameState = e.Data;
+                ITraceWriter traceWriter = new MemoryTraceWriter();
+                try
+                {
+                    Debug.WriteLine("STATE " + counter +": " + gameState);
+                    currentState = JsonConvert.DeserializeObject<RoboData>(gameState);
+                    Debug.WriteLine("DATA" + currentState.p2.mainCore.owner);
+
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    Debug.WriteLine("TRACE"+traceWriter+"\n");
+                }
+                finally
+                {
+
+                    if (counter++ > maxLines)
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+            };
 
             socket.Connect();
             socket.Send(devkey);
 
             Console.ReadLine();
 
+            //output.Write(Encoding.ASCII.GetBytes(gameState), 0, Encoding.ASCII.GetByteCount(gameState));
+
+            output.Close();
+
             socket.Close();
-
-
-
         }
     }
 }
---
+++ RoboGeneral6000/RoboGeneral6000/RoboData.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+
+
+public class Unit
+{
+    public int owner { get; set; }
+    public int x { get; set; }
+    public int y { get; set; }
+    public int maxhp { get; set; }
+    public int hp { get; set; }
+    public int @enum { get; set; }
+
+    public Unit(string option)
+    {
+        if(String.Equals(option, "nil"))
+        {
+            owner = 0;
+            x = 0;
+            y = 0;
+            maxhp = 0;
+            hp = 0;
+            @enum = -1212;
+        }
+    }
+
+    public string PrintData()
+    {
+        string result = "";
+
+        result += "    OWNER" + owner.ToString() + "\n";
+        result += "    X" + x.ToString() + "\n";
+        result += "    Y" + y.ToString() + "\n";
+        result += "    MAXHP" + maxhp.ToString() + "\n";
+        result += "    HP" + hp.ToString() + "\n";
+        result += "    ENUM" + @enum.ToString() + "\n";
+
+        return result;
+    }
+}
+
+public class MainCore
+{
+    public int owner { get; set; }
+    public int x { get; set; }
+    public int y { get; set; }
+    public int maxhp { get; set; }
+    public int hp { get; set; }
+    public int @enum { get; set; }
+
+    public string PrintData()
+    {
+        string result = "";
+
+        result += "    OWNER" + owner.ToString() + "\n";
+        result += "    X" + x.ToString() + "\n";
+        result += "    Y" + y.ToString() + "\n";
+        result += "    MAXHP" + maxhp.ToString() + "\n";
+        result += "    HP" + hp.ToString() + "\n";
+        result += "    ENUM" + @enum.ToString() + "\n";
+
+        return result;
+    }
+}
+
+public class Player
+{
+    public int owner { get; set; }
+    public int income { get; set; }
+    public int bits { get; set; }
+    public int horizonMin { get; set; }
+    public int horizonMax { get; set; }
+    public int territoryMin { get; set; }
+    public int territoryMax { get; set; }
+    public List<string> towers { get; set; }
+    public List<Unit> troops { get; set; }
+    public MainCore mainCore { get; set; }
+
+    public string PrintData()
+    {
+        string result;
+
+        result = "  OWNER: " + owner.ToString() + "\n";
+        result += "  INCOME: " + income.ToString() + "\n";
+        result += "  BITS: " + bits.ToString() + "\n";
+        result += "  HORIZON_MIN: " + horizonMin.ToString() + "\n";
+        result += "  HORIZON_MAX: " + horizonMax.ToString() + "\n";
+        result += "  TERRITORY_MIN: " + territoryMin.ToString() + "\n";
+        result += "  TERRITORY_MAX: " + territoryMax.ToString() + "\n";
+
+        for(int itr = 0; itr<towers.Count; itr++)
+        {
+            result += "  " + towers[itr] + "\n";
+        }
+
+        for(int troop = 0; troop<troops.Count; troop++)
+        {
+            result += "  " + troops[troop].PrintData() + "\n";
+        }
+
+
+        result += "  MAINCORE: " + mainCore.PrintData() + "\n";
+
+        return result;
+    }
+}
+
+public class RoboData
+{
+    public int w { get; set; }
+    public int h { get; set; }
+    public Player p1 { get; set; }
+    public Player p2 { get; set; }
+
+    public string PrintData()
+    {
+        string result;
+        result = "WIDTH: " + w.ToString() + "\n HEIGHT: " + h.ToString() + "\n";
+        result += "P1: \n" + p1.PrintData();
+        result += "P2: \n" + p2.PrintData();
+        return result;
+    }
+}
+
+
+/*public class Troop
+{
+    public int owner { get; set; }
+    public int x { get; set; }
+    public int y { get; set; }
+    public int maxhp { get; set; }
+    public int hp { get; set; }
+    public int @enum { get; set; }
+}
+
+public class MainCore
+{
+    public int owner { get; set; }
+    public int x { get; set; }
+    public int y { get; set; }
+    public int maxhp { get; set; }
+    public int hp { get; set; }
+    public int @enum { get; set; }
+}
+
+public class P1
+{
+    public int owner { get; set; }
+    public int income { get; set; }
+    public int bits { get; set; }
+    public int horizonMin { get; set; }
+    public int horizonMax { get; set; }
+    public int territoryMin { get; set; }
+    public int territoryMax { get; set; }
+    public List<string> towers { get; set; }
+    public List<Troop> troops { get; set; }
+    public MainCore mainCore { get; set; }
+}
+
+public class MainCore2
+{
+    public int owner { get; set; }
+    public int x { get; set; }
+    public int y { get; set; }
+    public int maxhp { get; set; }
+    public int hp { get; set; }
+    public int @enum { get; set; }
+}
+
+public class P2
+{
+    public int owner { get; set; }
+    public int income { get; set; }
+    public int bits { get; set; }
+    public int horizonMin { get; set; }
+    public int horizonMax { get; set; }
+    public int territoryMin { get; set; }
+    public int territoryMax { get; set; }
+    public List<string> towers { get; set; }
+    public List<object> troops { get; set; }
+    public MainCore2 mainCore { get; set; }
+}
+
+public class RoboData
+{
+    public int w { get; set; }
+    public int h { get; set; }
+    public P1 p1 { get; set; }
+    public P2 p2 { get; set; }
+}
+*/
