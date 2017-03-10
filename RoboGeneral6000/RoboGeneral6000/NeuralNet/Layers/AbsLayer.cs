66352c3d0f4ff2f9a2447a56e7518bfcb2581d79
Aaron Brooks <Aaron Brooks>
2017-03-10 13:43:53 -0500

    Adds Basic layer support, should be sufficient to begin building structure to manipulate net

---
+++ RoboGeneral6000/RoboGeneral6000/NeuralNet/Layers/AbsLayer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboGeneral6000.NeuralNet
+{
+    abstract class AbsLayer
+    {
+        protected double[] values;
+        protected double[][] weights;
+        protected int valCount;
+
+        protected AbsLayer NextLayer;
+
+        public AbsLayer()
+        {
+            valCount = 10;
+            weights = new double[valCount][];
+            for (int x = 0; x < valCount; x++)
+            {
+                weights[x] = new double[valCount];
+            }
+        }
+
+        public AbsLayer(int _size)
+        {
+            valCount = _size;
+            weights = new double[valCount][];
+            for (int x = 0; x < valCount;x++)
+            {
+                weights[x] = new double[valCount];
+            }
+        }
+
+        public virtual void ProcessLayer(double[] _input)
+        {
+            values = _input;
+            double[] output = WeightValues();
+            NextLayer.ProcessLayer(output);
+
+        }
+
+        protected double[] WeightValues()
+        {
+            double sum;
+            double[] output = new double[valCount];
+            for(int x = 0;x<valCount;x++)
+            {
+                for(int y=0; y<valCount;y++)
+                {
+                    sum = 0;
+                    for(int z = 0; z<weights[y].Length; z++)
+                    {
+                        sum += values[x] * weights[y][z];
+                    }
+                    output[x] = sum;
+                }
+            }
+            return output;
+        }
+
+    }
+}
---
+++ RoboGeneral6000/RoboGeneral6000/NeuralNet/Layers/OutputLayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboGeneral6000.NeuralNet.Layers
+{
+    class OutputLayer : AbsLayer
+    {
+// private double[] values;
+
+
+        override public void ProcessLayer(double[] _input)
+        {
+            values = _input;
+        }
+    }
+}
