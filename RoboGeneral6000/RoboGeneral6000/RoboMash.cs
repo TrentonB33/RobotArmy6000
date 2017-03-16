efc909367194271cb14638fb2fb507fc29011820
Trenton Baldrey <trentmant@gmail.com>
2017-03-15 21:40:45 -0400

    adds reproduction code.

--- RoboGeneral6000/RoboGeneral6000/RoboMash.cs
+++ RoboGeneral6000/RoboGeneral6000/RoboMash.cs
@@ -33,14 +33,22 @@ namespace RoboGeneral6000
         //Of members and create the next generation
         public static List<NeuralNet> Mash(List<NeuralNet> population)
         {
-            population.Sort(new FitnessComparator());
-            List<NeuralNet> newPop;
-            newPop = KillHalf(population);
+            try
+            {
+                population.Sort(new FitnessComparator());
+                List<NeuralNet> newPop;
+                newPop = KillHalf(population);
 
-            newPop = ReproducePop(newPop);
+                newPop = ReproducePop(newPop);
 
-            
-            return newPop;
+                return newPop;
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+            return population;
         }
 
 
@@ -91,54 +99,67 @@ namespace RoboGeneral6000
             NeuralNet first = null;
             NeuralNet second = null;
 
-            List<int> board = GenBoard(toReproduce);
+            List<double> board = GenBoard(toReproduce);
 
             Random randGen = new Random((int)DateTime.Now.Ticks);
             int numRuns = toReproduce.Count;
             double firstRand = 0;
             double secondRand = 0;
 
+            Debug.WriteLine("Board Size: " + board.Count.ToString());
+            Debug.WriteLine("To Reproduce Size: " + toReproduce.Count.ToString());
+
             //First, select two members and recombine the member genes if needed
-            for(int round = 0; round < numRuns; round++)
+            for (int round = 0; round < numRuns; round++)
             {
                 first = null;
                 second = null;
                 firstRand = randGen.NextDouble();
                 secondRand = randGen.NextDouble();
 
+                Debug.WriteLine("Rooouuunnnndddd: " + round.ToString());
+                
+
                 for (int itr = 0; itr < board.Count; itr++)
                 {
-                    if (board[itr] < firstRand)
+                    Debug.WriteLine("Iteration: " + itr.ToString());
+                    
+                    if (board[itr] < firstRand && firstRand < board[itr + 1])
                     {
                         first = toReproduce[itr];
                     }
-                    if(board[itr] < secondRand)
+                    if(board[itr] < secondRand && secondRand < board[itr + 1])
                     {
                         second = toReproduce[itr];
                     }
+                    Debug.WriteLine("Do I make it here?");
                     if(first != null && second != null)
                     {
                         break;
                     }
+                    Debug.WriteLine("How about here?");
                 }
-                if (randGen.NextDouble() > recombineProbability)
+                
+                if (randGen.NextDouble() < recombineProbability)
                 {
+                    Debug.WriteLine("Entering reproduce");
                     RecombineMembers(first, second, newPop, randGen);
                 }
                 else
                 {
+                    Debug.WriteLine("Adding memebers after no recombo");
                     newPop.Add(new NeuralNet(first));
                     newPop.Add(new NeuralNet(second));
                 }
 
             }
 
-
             return newPop;
         }
 
         private static void RecombineMembers(NeuralNet first, NeuralNet second, List<NeuralNet> newPop, Random randGen)
         {
+            Debug.WriteLine("In the butt");
             NeuralNet clone1 = new NeuralNet(first);
             NeuralNet clone2 = new NeuralNet(second);
 
@@ -148,12 +169,19 @@ namespace RoboGeneral6000
             int crossIndex = randGen.Next(0, numEdges);
             double tempWeight = 0;
 
+            Debug.WriteLine("Cross Index Value: " + crossIndex.ToString());
+            clone1.PrintNet();
+            clone2.PrintNet();
+
             for(int index = crossIndex; index < numEdges; index++)
             {
-                tempWeight = clone1.GetEdge(index);
-                clone1.UpdateEdge(index, clone2.GetEdge(index));
+                tempWeight = clone1.getEdge(index);
+                clone1.UpdateEdge(index, clone2.getEdge(index));
                 clone2.UpdateEdge(index, tempWeight);
             }
+
+            clone1.PrintNet();
+            clone2.PrintNet();
 
             newPop.Add(clone1);
             newPop.Add(clone2);
@@ -164,36 +192,32 @@ namespace RoboGeneral6000
 
         //Helper Functions
 
-        private static List<int> GenBoard(List<NeuralNet> population)
+        private static List<double> GenBoard(List<NeuralNet> population)
         {
-            List<int> board = new List<int>();
+            List<double> board = new List<double>();
             int sum = 0;
+            double otherSum = 0;
 
             for(int i = 0; i < population.Count; i++)
             {
                 sum += population[i].fitness;
             }
+
+            Debug.WriteLine("Sum: "+sum.ToString());
+            board.Add(0);
             for (int i = 0; i < population.Count; i++)
             {
-                board.Add(population[i].fitness / sum);
+                otherSum += (double)population[i].fitness / sum;
+                board.Add(otherSum);
+                Debug.Write(board[i].ToString() + " ");
             }
 
+            Debug.Write(board[board.Count-1].ToString() + " ");
+            Debug.WriteLine("");
             Debug.WriteLine(board.Count.ToString());
 
             return board;
         }
-
-        
-
-
-
-
-
-
-
-
-
-
 
 
         //For debugging
@@ -201,11 +225,11 @@ namespace RoboGeneral6000
         {
             List<NeuralNet> sample = new List<NeuralNet>();
 
-            int num = 50;
+            int num = 10;
 
             for(int i = 0; i < num; i++)
             {
-                sample.Add(new NeuralNet());
+                sample.Add(new NeuralNet(2,5,3,3,.75));
                 sample[i].fitness = i;
             }
 
